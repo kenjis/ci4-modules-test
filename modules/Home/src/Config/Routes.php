44ecb00875aead3d240f09f81f50338ce94dec3e
kenjis <kenji.uui@gmail.com>
2023-01-23 20:52:13 +0900

    feat: parser sample
    http://localhost:8080/parser

--- modules/Home/src/Config/Routes.php
+++ modules/Home/src/Config/Routes.php
@@ -4,3 +4,4 @@ namespace Home\Config;
 
 $routes->get('/', [\Home\Controllers\Home::class, 'index']);
 $routes->get('/helper', [\Home\Controllers\Home::class, 'helper']);
+$routes->get('/parser', [\Home\Controllers\Home::class, 'parser']);
