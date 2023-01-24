9686abcda992a80b3c5b6f483b39101e8a3e523f
kenjis <kenji.uui@gmail.com>
2023-01-24 11:02:24 +0900

    feat: add parser with viewPath sample

--- modules/Home/src/Config/Routes.php
+++ modules/Home/src/Config/Routes.php
@@ -5,3 +5,4 @@ namespace Home\Config;
 $routes->get('/', [\Home\Controllers\Home::class, 'index']);
 $routes->get('/helper', [\Home\Controllers\Home::class, 'helper']);
 $routes->get('/parser', [\Home\Controllers\Home::class, 'parser']);
+$routes->get('/parser-viewpath', [\Home\Controllers\Home::class, 'parser_viewpath']);
--- modules/Home/src/Controllers/Home.php
+++ modules/Home/src/Controllers/Home.php
@@ -36,4 +36,23 @@ class Home extends BaseController
 
         return $parser->setData($data)->render('Home\Views\blog_template');
     }
+
+    public function parser_viewpath()
+    {
+        $parser = \Config\Services::parser(__DIR__ . '/../Views');
+
+        $data = [
+            'blog_title'   => 'My Blog Title',
+            'blog_heading' => 'My Blog Heading',
+            'blog_entries' => [
+                ['title' => 'Title 1', 'body' => 'Body 1'],
+                ['title' => 'Title 2', 'body' => 'Body 2'],
+                ['title' => 'Title 3', 'body' => 'Body 3'],
+                ['title' => 'Title 4', 'body' => 'Body 4'],
+                ['title' => 'Title 5', 'body' => 'Body 5'],
+            ],
+        ];
+
+        return $parser->setData($data)->render('blog_template');
+    }
 }
